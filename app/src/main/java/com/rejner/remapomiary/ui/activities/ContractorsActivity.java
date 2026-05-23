8ae90064bb448e70a007ad05031a0f8f7333ff21
xvmari <kozinkaa53@gmail.com>
2026-05-23 14:21:58 +0200

    widok wykanawcow

---
+++ app/src/main/java/com/rejner/remapomiary/ui/activities/ContractorsActivity.java
@@ -0,0 +1,22 @@
+package com.rejner.remapomiary.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.rejner.remapomiary.R;
+
+public class ContractorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contractors);
+
+    }
+}
